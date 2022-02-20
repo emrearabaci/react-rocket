6cc953eea3a00ddef639e694838c1d696ab8981e
Emre Arabaci <a.emrearabaci@gmail.com>
2022-02-20 23:20:11 +0300

    Create Navigation.js
    Created "Navigation.js" component for main layout.

---
+++ src/components/layout/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styles
+import styles from '../../styles/layout/navigation.module.scss';
+
+function Navigation() {
+  return (
+    <div className={styles.structure}>
+      <h2>navigation</h2>
+    </div>
+  );
+}
+
+export default Navigation;
