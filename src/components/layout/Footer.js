cf1a9fc6392afd5b2d2fc8e6396793b083398459
Emre Arabaci <a.emrearabaci@gmail.com>
2022-02-20 23:19:45 +0300

    Create Footer.js
    Created "Footer.js" component for main layout.

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styles
+import styles from '../../styles/layout/footer.module.scss';
+
+function Footer() {
+  return (
+    <div className={styles.structure}>
+      <h2>footer</h2>
+    </div>
+  );
+}
+
+export default Footer;
